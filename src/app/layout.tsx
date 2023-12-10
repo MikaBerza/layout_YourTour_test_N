817a1d2261d6f0254706c63f5c0e32a33af83335
Mika <mikberza@mail.ru>
2023-12-10 21:53:14 +0300

    Add (app): added (favicon) #1
    Добавить (app): добавлен (favicon) #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Header } from '../components/commons/Header';
 import { Footer } from '../components/commons/Footer';
 import './globals.css';
-
+import Head from 'next/head';
 export const metadata: Metadata = {
   description: 'Продажа туров для идеальных путешествий и отдыха',
   keywords:
@@ -19,6 +19,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ru'>
+      <Head>
+        <link rel='icon' href='./favicon.ico' sizes='any' />
+        <link
+          rel='apple-touch-icon'
+          href='../../public/images/fav_icons_YT/apple-touch-icon.png'
+          sizes='180x180'
+        />
+        <link
+          rel='icon'
+          href='../../public/images/fav_icons_YT/favicon-32x32.png'
+          type='image/png'
+          sizes='32x32'
+        />
+        <link
+          rel='icon'
+          href='../../public/images/fav_icons_YT/favicon-16x16.png'
+          type='image/png'
+          sizes='16x16'
+        />
+        <link rel='manifest' href='../../public/images/fav_icons_YT/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='../../public/images/fav_icons_YT/safari-pinned-tab.svg'
+          color='#3b7e7b'
+        />
+      </Head>
       <body>
         <Header />
         <main className='bgc-1'>{children}</main>
