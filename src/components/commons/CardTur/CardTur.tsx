ecd2d523ab33a2fe97fd788b307bff8b9e18540e
Mika <mikberza@mail.ru>
2023-12-13 00:44:29 +0300

    Fix (CardTur): shortened syntax, adding styles #10 #10 -using abbreviated syntax (JS) -removing the pseudo class (data-price) and adding a tag (p), adding styles
    Исправить (CardTur): сокращенный синтаксис, добавление стилей #10
    #10
    -использование сокращенного синтаксиса (JS)
    -удаление псевдо класса (data-price) и добавление тега (p), добавление стилей

--- src/components/commons/CardTur/CardTur.tsx
+++ src/components/commons/CardTur/CardTur.tsx
@@ -10,30 +10,31 @@ const CardTur: React.FC<{ cardTurData: CardTurPropsType[] }> = ({
 }) => {
   return (
     <div className={styles.wrapper}>
-      {cardTurData.map((item: CardTurPropsType) => {
-        return (
-          <div className={styles.inner} key={item.id}>
-            <Image
-              src={item.pathImg}
-              width={370}
-              height={531}
-              className={`${styles.img} bgp-1`}
-              alt='images'
-              priority
-            />
-            <div className={styles.content}>
+      {cardTurData.map((item: CardTurPropsType) => (
+        <div className={styles.inner} key={item.id}>
+          <Image
+            src={item.pathImg}
+            width={370}
+            height={531}
+            className={`${styles.img} bgp-1`}
+            alt='images'
+            priority
+          />
+          <div className={styles.content}>
+            <div>
               <h3 className={styles.title} data-price={item.price}>
                 {item.title}
               </h3>
-              <LinkButton
-                parentStyles={[styles.linkBtn, styles.linkItemBtn]}
-                link={item.link}
-                linkBtnName='Подробнее'
-              />
+              <p className={styles.price}>{item.price}</p>
             </div>
+            <LinkButton
+              parentStyles={[styles.linkBtn, styles.linkItemBtn]}
+              link={item.link}
+              linkBtnName='Подробнее'
+            />
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
