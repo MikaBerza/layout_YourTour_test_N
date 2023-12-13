6606cf891951b979391aa867e995f0d0c7be88f0
Mika <mikberza@mail.ru>
2023-12-13 15:08:07 +0300

    Fix (Subtitle): the default syntax for adding a props #19 #19 -fixed the syntax for adding a default props -the old syntax for adding a prop has been removed
    Исправить (Subtitle): синтаксис добавления пропса по умолчанию #19
    #19
    -исправлен синтаксис добавления пропса по умолчанию
    -старый синтаксис добавления пропса удален

--- src/components/commons/Subtitle/Subtitle.tsx
+++ src/components/commons/Subtitle/Subtitle.tsx
@@ -1,14 +1,10 @@
 import styles from './Subtitle.module.css';
 import { TitleElementsPropsType } from '../../../types/customType';
 
-const Subtitle = ({ nameStyles, text }: TitleElementsPropsType) => {
+const Subtitle = ({ nameStyles = 'minorText', text }: TitleElementsPropsType) => {
   return (
     <p className={`${styles[nameStyles]} ${styles.generalText}`}>{text}</p>
   );
-};
-
-Subtitle.defaultProps = {
-  nameStyles: 'minorText',
 };
 
 Subtitle.displayName = 'Subtitle';
